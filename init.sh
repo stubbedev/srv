93b484a620320aa1badcf56818b7f8dacfd19bad
Alexander Stage <alexander.bugge.stage@gmail.com>
2025-05-22 19:48:01 +0000

    create docker network

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ function init() {
 	cd $PWD/traefik
 	docker compose up -d
 	cd $current_dir
+	docker network inspect web >/dev/null 2>&1 || docker network create web
 	for d in sites/*/; do
   		[ -d "$d" ] && cd "$current_dir/$d" && docker compose up -d && cd $current_dir
 	done
